b89cdb0d62c30a8afc90aee609961aa21b8f4962
Samrat Sarkar <sarkarsamrat16112004@gmail.com>
2023-11-13 23:51:32 +0530

    Create loop1.cpp

---
+++ loops/loop1.cpp
@@ -0,0 +1,13 @@
+//printing pattern like 1,4,9........100
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,m;
+    for(i=1;i<=10;i++)
+    {
+        m=i*i;
+        cout<<"\n"<<m;
+    }
+    return 0;
+}
